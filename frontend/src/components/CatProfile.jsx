56620c35e63227c5c41b1a96c043913af357a2bd
lysabrina <lysabrinak@gmail.com>
2024-02-25 12:28:18 -0800

    First commit

---
+++ frontend/src/components/CatProfile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function CatProfile() {
+  return <div className="profile">
+    <div className="profile__img">
+
+    </div>
+    <div className="profile__info">
+        
+    </div>
+  </div>;
+}
