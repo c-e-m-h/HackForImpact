4d13ca1788e436388b94980a85cbfb06a035c974
Imra Dawoodani <imra.d@berkeley.edu>
2024-02-25 16:51:44 -0800

    apis

--- backend/app/api/views.py
+++ backend/app/api/views.py
@@ -1,13 +1,26 @@
 from flask import Blueprint, jsonify
 from backend.app.services import RescueGroupsAPI
-
+from backend.app.models import Cat, db_session
 
 bp = Blueprint('animals', __name__)
 
 @bp.route('/animals/<int:animal_id>')
 def get_animal(animal_id):
-    animal_details = RescueGroupsAPI.animal(animal_id)
-    if animal_details:
-        return jsonify(animal_details), 200
+    cat_data = RescueGroupsAPI.animal(animal_id)
+    if cat_data:
+        cat = Cat(
+            name=cat_data.name,
+            breed=cat_data.breed,
+            sex=cat_data.sex,
+            status=cat_data.status,
+            color=cat_data.color,
+            altered=cat_data.altered,
+            birthday=cat_data.birthday,
+            rescue_id=cat_data.rescueId,
+            picture_url=cat_data.picture_url
+        )
+        db_session.add(cat)
+        db_session.commit()
+        return jsonify(cat_data), 200
     else:
-        return jsonify({"error": "Animal not found"}), 404
+        return jsonify({"error": "Cat not found"}), 404
